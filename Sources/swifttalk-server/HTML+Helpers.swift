55a3155750d135fe001ea0f6e708e13e6177c3a9
Chris Eidhof <chris@eidhof.nl>
2018-11-12 16:53:28 +0100

    Button in a form

--- Sources/swifttalk-server/HTML+Helpers.swift
+++ Sources/swifttalk-server/HTML+Helpers.swift
@@ -13,6 +13,16 @@ extension Node {
         return Node.a(classes: classes, attributes: attributes, children, href: route.path)
     }
     
+    static func button(to route: Route, _ children: [Node], classes: Class? = nil, attributes: [String:String] = [:], confirm: String? = "Are you sure?") -> Node {
+        var attrs = ["type": "submit"]
+        if let c = confirm {
+            attrs["data-confirm"] = c
+        }
+        return Node.form(classes: "button_to", action: route.path, method: .post, [
+            Node.button(classes: classes, attributes: attrs, children)
+        ])
+    }
+    
     static func inlineSvg(path: String, preserveAspectRatio: String? = nil, classes: Class? = nil, attributes: [String:String] = [:]) -> Node {
         let name = resourcePaths.resolve("images/" + path)!
         var a = attributes
