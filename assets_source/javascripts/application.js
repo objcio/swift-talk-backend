0a87040fb9c899a47bd627c856ab41cfd33acbd6
Chris Eidhof <chris@eidhof.nl>
2018-11-16 13:51:56 +0100

    Require underscore (necessary for objcjs).

--- assets_source/javascripts/application.js
+++ assets_source/javascripts/application.js
@@ -1,9 +1,22 @@
+window.$ = window.jQuery = require('jquery');
+
+window._ = require('underscore'); // necessary for some of the objcjs plugins...
+require('./objcjs/plugins/scrollTo.jquery.js');
+require('./objcjs/plugins/_selectText.js');
+require('./objcjs/utilities/scrollTo.jquery.js');
+require('./objcjs/utilities/expandable.jquery.js');
+require('./objcjs/utilities/outbound-links.jquery.js');
+require('./objcjs/utilities/closeable.jquery.js');
+require('./objcjs/utilities/activatable.jquery.js');
+require('./objcjs/utilities/toggle.jquery.js');
+require('./objcjs/components/_code-selection.js');
+require('./objcjs/components/_scroller.js');
 require('./objcjs/index.js');
+
 require('./app-components.js');
 
 // plugins
 // todo: require('underscore/underscore');
-window.$ = window.jQuery = require('jquery');
 require('jquery.kinetic/jquery.kinetic');
 
 window.reframe = require('reframe.js');
