d7437f593b6b951034f93556494c070c8eea68af
Chris Eidhof <chris@eidhof.nl>
2018-11-23 12:55:55 +0100

    md5 package

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
 		.package(url: "https://github.com/vapor-community/postgresql.git", .exact("2.1.2")),
         .package(url: "https://github.com/chriseidhof/commonmark-swift", .branch("master")),
         .package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", .exact("3.1.3")),
+		.package(url: "https://github.com/IBM-Swift/BlueCryptor", .exact("1.0.20")),
     ],
     targets: [
         .target(
@@ -21,6 +22,7 @@ let package = Package(
 				"PostgreSQL",
                 "CommonMark",
                 "PerfectXML",
+				"Cryptor",
 		]),
     ]
 )
