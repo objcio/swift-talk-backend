096d65300ff78aced39677ff478af3f09ca5d09e
Florian Kugler <mail@floriankugler.com>
2018-12-06 13:27:30 +0100

    Fix episode release task scheduling

--- Sources/swifttalk-server/Static/StaticData.swift
+++ Sources/swifttalk-server/Static/StaticData.swift
@@ -38,7 +38,7 @@ final class Static<A> {
 fileprivate let episodesSource: Static<[Episode]> = .fromStaticRepo(onRefresh: { newEpisodes in
     for ep in newEpisodes where ep.releaseAt > Date() {
         let query = Task.releaseEpisode(number: ep.number).schedule(at: ep.releaseAt)
-        tryOrLog("Failed to schedule release task for episode \(ep.number)") { try lazyConnection().get().execute(query) }
+        tryOrLog("Failed to schedule release task for episode \(ep.number)") { try withConnection { try $0.execute(query) } }
     }
 })
 
