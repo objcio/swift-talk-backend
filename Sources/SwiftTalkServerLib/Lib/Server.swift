4514ad845bcbcd60a1a0a7199750c08324b1c359
Florian Kugler <mail@floriankugler.com>
2019-01-31 12:53:00 +0100

    Remove print

--- Sources/SwiftTalkServerLib/Lib/Server.swift
+++ Sources/SwiftTalkServerLib/Lib/Server.swift
@@ -182,7 +182,6 @@ extension String {
 
 extension StringProtocol {
     var parseAsQueryPart: [String:String] {
-        print(self)
         let items = split(separator: "&").compactMap { $0.keyAndValue }
         return Dictionary(items.map { (k,v) in (k.decoded, v.decoded) }, uniquingKeysWith: { $1 })
     }
