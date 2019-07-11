860ca93dcbf8afe07b91a173b5fb3479da1b3d5f
Florian Kugler <mail@floriankugler.com>
2019-07-11 12:23:03 +0200

    Add encoding test

--- Tests/swifttalkTests/TestRecurly.swift
+++ Tests/swifttalkTests/TestRecurly.swift
@@ -45,11 +45,30 @@ let activateSubscription = """
 </new_subscription_notification>
 """
 
+fileprivate let createSubXML = """
+<?xml version="1.0" encoding="UTF-8"?>
+<subscription>
+<plan_code>monthly-test</plan_code>
+<currency>USD</currency>
+<account>
+<account_code>241A52B6-CE23-4B6A-90B7-52710F3E6312</account_code>
+<email>mail@floriankugler.com</email>
+<billing_info>
+<token_id>O_zV7-8FNW6gYR0tXqrxjA</token_id>
+</billing_info>
+</account>
+</subscription>
+"""
 
 class RecurlyTests: XCTestCase {
     func testNewSubscriptionNotification() throws {
         let webhook: Webhook = try decodeXML(from: activateSubscription.data(using: .utf8)!)
         let expectedPlan = Webhook.Plan(plan_code: "gift_three_months", name: "Gift Three Months")
         XCTAssertEqual(webhook.subscription?.plan, expectedPlan)
+    }
+    
+    func createSubscriptionXML() throws {
+        let x = CreateSubscription(plan_code: "monthly-test", currency: "USD", coupon_code: nil, starts_at: nil, account: CreateSubscription.CreateAccount(account_code: UUID(uuidString: "241A52B6-CE23-4B6A-90B7-52710F3E6312")!, email: "mail@floriankugler.com", billing_info: CreateSubscription.CreateBillingInfo(token_id: "O_zV7-8FNW6gYR0tXqrxjA")))
+        XCTAssertEqual(try encodeXML(x), createSubXML)
     }
 }
