65eedcb43274d15ec72ead745c5f3bde38e6bcd6
Chris Eidhof <chris@eidhof.nl>
2019-07-16 10:56:50 +0200

    Use shared view helpers

--- Sources/SwiftTalkServerLib/Views/ViewHelpers.swift
+++ Sources/SwiftTalkServerLib/Views/ViewHelpers.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Base
+@_exported import ViewHelpers
 
 extension Optional where Wrapped == Session {
     var premiumAccess: Bool {
@@ -35,78 +36,6 @@ extension String {
 extension String {
     func pluralize(_ number: Int) -> String {
         return number == 1 ? self : "\(self)s"
-    }
-}
-
-extension Int {
-    var padded: String {
-        return self < 10 ? "0" + "\(self)" : "\(self)"
-    }
-}
-
-extension DateFormatter {
-    static let iso8601: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return dateFormatter
-    }()
-}
-
-extension TimeInterval {
-    private var hm: (Int, Int, Int) {
-        let h = floor(self/(60*60))
-        let m = floor(self.truncatingRemainder(dividingBy: 60*60)/60)
-        let s = self.truncatingRemainder(dividingBy: 60).rounded()
-        return (Int(h), Int(m), Int(s))
-    }
-    
-    var minutes: String {
-        let m = Int((self/60).rounded())
-        return "\(m) min"
-    }
-    
-    var hoursAndMinutes: String {
-        let (hours, minutes, _) = hm
-        if hours > 0 {
-            return "\(Int(hours))h\(minutes.padded)min"
-        } else { return "\(minutes)min" }
-    }
-    
-    var timeString: String {
-        let (hours, minutes, seconds) = hm
-        if hours == 0 {
-            return "\(minutes.padded):\(seconds.padded)"
-        } else {
-            return "\(hours):\(minutes.padded):\(seconds.padded)"
-        }
-    }
-}
-
-extension DateFormatter {
-    convenience init(format: String) {
-        self.init()
-        self.locale = Locale(identifier: "en_US")
-        self.timeZone = TimeZone(secondsFromGMT: 0)
-        self.dateFormat = format
-    }
-    
-    static let withYear = DateFormatter(format: "MMM dd yyyy")
-    
-    /// e.g. "November 23, 2018"
-    static let fullPretty = DateFormatter(format: "MMMM dd, yyyy")
-    static let withoutYear = DateFormatter(format: "MMM dd")
-}
-
-extension Date {
-    var pretty: String {
-        let cal = NSCalendar.current
-        if cal.component(.year, from: globals.currentDate()) == cal.component(.year, from: self) {
-            return DateFormatter.withoutYear.string(from: self)
-        } else {
-            return DateFormatter.withYear.string(from: self)
-        }
     }
 }
 
