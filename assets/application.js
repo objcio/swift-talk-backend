d8510b1b27824f7f44846aff68bea63d3aeebfa9
Florian Kugler <mail@floriankugler.com>
2019-07-19 08:32:04 +0200

    Regenerate js

--- assets/application.js
+++ assets/application.js
@@ -22,33 +22,6 @@ require('./objcjs/index.js');
 
 require('./app-components.js');
 
-// timestamp links
-
-$(function () {
-    window.player = new Vimeo.Player(document.querySelector('iframe'));
-
-    $('.js-transcript').find("a[href^='#']").each(function () {
-        if (/^\d+$/.test(this.hash.slice(1)) && /^\d{1,2}(:\d{2}){1,2}$/.test(this.innerHTML)) {
-            var time = parseInt(this.hash.slice(1));
-            $(this).data('time', time).attr('href', '?t=' + time).addClass('js-episode-seek js-transcript-cue');
-        }
-    });
-
-    // Auto-expand transcript if #transcript hash is passed
-    if (window.location.hash.match(/^#?transcript$/)) {
-        $('#transcript').find('.js-expandable-trigger').trigger('click');
-    }
-
-    // Catch clicks on timestamps and forward to player
-    $(document).on('click singletap', '.js-episode .js-episode-seek', function (event) {
-        if ($(this).data('time') !== undefined) {
-            player.setCurrentTime($(this).data('time'));
-            player.play();
-            event.preventDefault();
-        }
-    });
-});
-
 },{"./app-components.js":1,"./objcjs/index.js":14,"./objcjs/plugins/scrollTo.jquery.js":15,"./objcjs/utilities/scrollTo.jquery.js":16,"jquery":44}],3:[function(require,module,exports){
 'use strict';
 
