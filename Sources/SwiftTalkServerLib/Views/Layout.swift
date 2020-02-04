b01371aef643595d0d53c8104c729fdc0dc132f0
Chris Eidhof <chris@eidhof.nl>
2020-02-04 11:22:45 +0100

    Remove emoji

--- Sources/SwiftTalkServerLib/Views/Layout.swift
+++ Sources/SwiftTalkServerLib/Views/Layout.swift
@@ -95,7 +95,7 @@ let navigationItems: [(LinkTarget, String)] = [
     (Route.home, "Swift Talk"),
     (URL(string: "https://www.objc.io/books")!, "Books"),
     (URL(string: "https://www.objc.io/issues")!, "Issues"),
-    (URL(string: "https://www.objc.io/blog/2018/12/11/swift-talk-gift-subscription/")!, "🎁 Gifts"),
+    (URL(string: "https://www.objc.io/blog/2018/12/11/swift-talk-gift-subscription/")!, "Gifts"),
 ]
 
 let rssURL = Route.rssFeed.url.absoluteString
